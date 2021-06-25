6306eec6b9b1c1b827bec1df4b6bca5af624f755
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-06-26 00:26:06 +0900

    Complete crawl_search_options.py

---
+++ crawl_search_options.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3 
+import json
+
+from selenium import webdriver
+
+
+area_map = ['tyo', 'spk', 'sdj', 'tyo', 'ngo', 'osa', 'hij', 'fuk']
+
+def getOptionValueTextPair(driver, input_name):
+    info_key = "{ 'text': x.getAttribute('data-text'), 'value': x.value }"
+
+    js_query = """
+        return Array.from(
+                document.querySelectorAll(
+                    'input[name="{}"]'
+                )
+            ).map(function(x) {{
+                return {}
+            }})
+    """
+    return driver.execute_script(js_query.format(input_name, info_key))
+
+
+def crawlSearchOptions(area_id):
+    base_url = f"https://bus-tour.his-j.com/{area_id}/search/"
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--single-process')
+    chrome_options.add_argument('--disable-application-cache')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument('--hide-scrollbars')
+    chrome_options.add_argument('--enable-logging')
+    chrome_options.add_argument('--log-level=0')
+    chrome_options.add_argument('--ignore-certificate-errors')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(base_url)
+    driver.implicitly_wait(10)
+
+    departure_area_info = getOptionValueTextPair(driver, "departureArea[]")
+    purpose_info = getOptionValueTextPair(driver, "purpose[]")
+    destination_info = getOptionValueTextPair(driver, "destination[]")
+    tour_type_info = getOptionValueTextPair(driver, "type[]")
+
+    driver.close()
+
+    # '' means free-style
+    return {
+        'departure_area_options': departure_area_info,
+        'departureDate': '',
+        'departureMonth': '',
+        'purpose_options': purpose_info,
+        'destination_options': destination_info,
+        'tour_type_options': tour_type_info,
+        'lowPrice': '',
+        'highPrice': '',
+        'isGoOnly': [True, False],
+        'isReservable': [True, False],
+        'keyword': ''
+    }
+
+
+if __name__== '__main__':
+    for area_id in area_map:
+        search_option_db = {}
+        search_option_db[area_id] = crawlSearchOptions(area_id)
+
+        with open('{}_search_option_db.json'.format(area_id), 'w') as dst:
+            json.dump(search_option_db, dst, ensure_ascii=False, indent=4)
+
